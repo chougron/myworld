909a982b8d09a35721990967830c5e5174952173
Camille Hougron <camille.hougron@gmail.com>
2018-08-27 22:54:12 +0200

    Forgot file

--- src/game/client/src/reducers/index.ts
+++ src/game/client/src/reducers/index.ts
@@ -1,14 +1,6 @@
 import { combineReducers } from 'redux';
 import characters from './character';
 import player from './player';
-import ICharacter from '../../../../shared/types/character';
-
-export type State = {
-    characters: ICharacter[];
-    player: {
-        character: ICharacter;
-    };
-};
 
 const reducers = combineReducers({ characters, player });
 
